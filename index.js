ad6aaa2dcc9fc9cc01dc83b5e060ce6f6f7605d4
Victor Haynes <haynesvic@gmail.com>
2022-06-27 18:44:44 -0500

    commit tree e13359eaf2f5a6c380c54ce2ee31d65dae35c49e

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+
+//
+let random = Math.ceil(Math.random())
+
+//
+let num3 = 16
+let num4 = 12
+let mod = num3 % num4
+
+//
+let max = Math.max(1,2,3,20)
